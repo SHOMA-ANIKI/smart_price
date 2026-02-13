c39669de183d9081a34cada91649f20f7a532928
Shamil <shamil004015@gmail.com>
2026-02-13 14:08:16 +0300

    Add BaseRepository with generic CRUD methods

---
+++ src/repositories/base.py
@@ -0,0 +1,21 @@
+
+from typing import TypeVar, Generic, Type, Optional, Sequence
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+T = TypeVar("T")
+
+class BaseRepository(Generic[T]):
+    def __init__(self, model: Type[T], session: AsyncSession):
+        self.model = model
+        self.session = session
+
+    async def find_one_or_none(self, **filter_by) -> Optional[T]:
+        stmt = select(self.model).filter_by(**filter_by)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def add(self, data: dict) -> T:
+        stmt = insert(self.model).values(**data).returning(self.model)
+        result = await self.session.execute(stmt)
+        return result.scalar_one()
