19140f8c3423f4fc1adf013d32353b3d55d76210
Shamil <shamil004015@gmail.com>
2026-02-13 14:09:03 +0300

    Add ProductRepository inherited from BaseRepository

---
+++ src/repositories/products.py
@@ -0,0 +1,12 @@
+
+from src.models import Product
+from src.repositories.base import BaseRepository
+from sqlalchemy import update
+
+class ProductRepository(BaseRepository[Product]):
+    def __init__(self, session):
+        super().__init__(Product, session)
+
+    async def update_price(self, product_id: int, new_price: float):
+        stmt = update(Product).where(Product.id == product_id).values(price=new_price)
+        await self.session.execute(stmt)
